9a7c8d6073f641ef57215f82bd52ef8a05a42f87
Yuta-Morishita <yuta.morishita@gmail.com>
2020-02-19 10:22:55 +0900

    アソシエーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :items
+  has many :points dependent: :destroy
+  has_one  :deliver_adresses dependent: :destroy
 
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :password, :email, presence: true
 end
