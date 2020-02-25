ac2cdb43eeeed9ce517dd179d7a6a67a9aa6b3e8
taku1115 <59477643+taku1115@users.noreply.github.com>
2020-02-25 12:05:38 +0900

    Merge branch 'master' of https://github.com/taku1115/freemarket_sample_68e

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_033808) do
+ActiveRecord::Schema.define(version: 2020_02_24_074934) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "category_id", null: false
