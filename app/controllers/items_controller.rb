f5107705e672aa78a0b647c80d16f2601685a04c
kyo hayashida <kanayan-love.1127@docomo.ne.jp>
2020-02-25 11:01:31 +0900

    controller　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   
   def index
     @items =Item.order("id Asc").limit(4)
-    @item_images = ItemImage.all
     @parents =Category.where(ancestry: nil)
   end
 
