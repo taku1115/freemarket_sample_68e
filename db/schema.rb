786313a9097e8413ae534eb68bde72f3f8df70bf
kei53 <kaiji303@gmail.com>
2020-02-26 17:36:40 +0900

    exhibitionコンフリクト

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_26_064418) do
-
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "category_id", null: false
     t.string "name", null: false
