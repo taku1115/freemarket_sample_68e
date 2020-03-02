5f31615ead2857ab7fb2302b6383ba3ac016b0b1
Yuta-Morishita <yuta.morishita@gmail.com>
2020-03-02 20:02:07 +0900

    編集調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
   belongs_to :brand,optional:true
   has_many :item_images, dependent: :destroy
   belongs_to :size,optional:true
-  accepts_nested_attributes_for :item_images, :shipping, :category, allow_destroy: true
+  accepts_nested_attributes_for :item_images, allow_destroy: true
+  accepts_nested_attributes_for :shipping, update_only: true
+  accepts_nested_attributes_for :category
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
 
