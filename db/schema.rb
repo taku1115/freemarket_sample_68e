44678667de8b8898d48c549720a9108f52a887d7
taku1115 <59477643+taku1115@users.noreply.github.com>
2020-03-03 15:17:11 +0900

    バリデーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_030517) do
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "item_id"
-    t.text "text"
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
